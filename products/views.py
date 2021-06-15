fbbdaa14fe83d23634a12f78987341b3781ac71f
Volodymyr Moruhin <vova.morugin@gmail.com>
2021-06-14 20:04:39 -0700

    minor changes

--- products/views.py
+++ products/views.py
@@ -11,6 +11,7 @@ import django_filters
 
 # Create your views here.
 
+
 class CategoryList(generics.ListAPIView):
     serializer_class = CategoryListSerializer
     queryset = Category.objects.all()
@@ -32,6 +33,7 @@ class CategoryRetrive(generics.RetrieveAPIView):
     serializer_class = CategoryRetriveSerializer
     queryset = Category.objects.all()
 
+
 class ProductRetrive(generics.RetrieveAPIView):
     serializer_class = ProductRetriveSerializer
     queryset = Product.objects.all()
@@ -46,25 +48,20 @@ class ProductListFromCategory(generics.ListAPIView):
         return Product.objects.filter(pk__in=product_ids)
 
 
-
-
-
 class ProductList(generics.ListAPIView):
     serializer_class = ProductPreviewSerializer
     queryset = Product.objects.all()
     pagination_class = ProductPagination
-    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filter_backends = [DjangoFilterBackend,
+                       filters.SearchFilter, filters.OrderingFilter]
     filterset_class = ProductFilter
     # filterset_fields = ['brand', 'price']
     search_fields = ['title']
     ordering_fields = ['title']
-    
-
 
 
 class BrandRetrieve(generics.RetrieveAPIView):
     serializer_class = BrandRetriveWithPruductSerializer
     queryset = Brand.objects.all()
 
-
-
+# def test_view(request):
