a311bd2b595c6a6331e5010170addb746459889a
Volodymyr Moruhin <vova.morugin@gmail.com>
2021-08-05 14:49:49 -0700

    deployment

--- core/settings.py
+++ core/settings.py
@@ -16,7 +16,7 @@ from pathlib import Path
 from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -149,3 +149,6 @@ SIMPLE_JWT = {
 STATICFILES_DIR = '/static/'
 STATIC_ROOT = '/static/'
 BASE_DOMAIN = 'http://127.0.0.1:8000'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
